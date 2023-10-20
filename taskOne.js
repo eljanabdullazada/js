30d75752117097ceaf87e371fa746963c8b33c0d
Elcan <elcan@Elcans-MacBook-Air-2.local>
2023-10-21 02:39:11 +0400

    Added readline objests

---
+++ taskOne.js
@@ -0,0 +1,13 @@
+const readline = require('readline-sync');
+
+const fullName = readline.question("Please enter your first name: ");
+const courseGrade = parseFloat(readline.question("Please enter your course grade: "));
+
+console.log("Good morning " + fullName);
+
+if (courseGrade >= 59.5) {
+    console.log("You passed the course!");
+    console.log(courseGrade);
+} else {
+    console.log("You failed the course");
+}
